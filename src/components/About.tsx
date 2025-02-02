db5740f1227d2857fb405601de034c59ea84fffd
Adam7domanski <adam7domanski@gmail.com>
2025-02-02 18:56:18 +0100

    libreries conflicts, resolved, libreries trimmed - material ui removed, timeline element added, work icon inserted to node element, icon colored white and scales properrly

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import aboutData from '../data/aboutData.json';
-import '../styles/About.module.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import workIcon  from '../resources/workIcon.svg';
+import 'react-vertical-timeline-component/style.min.css';
+import styles from '../styles/About.module.css';
 
 interface AboutData {
     dateFrom: string;
@@ -9,33 +12,33 @@ interface AboutData {
     description: string;
 }
 
-// Explicitly type the imported data
 const typedAboutData: AboutData[] = aboutData as AboutData[];
 
 const About: React.FC = () => {
     const [data, setData] = useState<AboutData[]>([]);
 
     useEffect(() => {
-        // Assuming aboutData.json is already imported and contains the data
         const sortedData = typedAboutData.sort((a: AboutData, b: AboutData) => new Date(b.dateFrom).getTime() - new Date(a.dateFrom).getTime());
         setData(sortedData);
     }, []);
 
     return (
-        <div className="about-container">
-            <div className="timeline">
-                {data.map((item, index) => (
-                    <div key={index} className="timeline-item">
-                        <div className="timeline-dot"></div>
-                        <div className="timeline-content">
-                            <h3>{item.dateFrom} - {item.dateTo}</h3>
-                            <h2>{item.title}</h2>
-                            <p>{item.description}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
+<VerticalTimeline>
+    {data.map((item, index) => (
+        <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            date={`${item.dateFrom} - ${item.dateTo}`}
+            iconStyle={{ background: 'rgb(33, 150, 243)'}}
+            icon={<img src={workIcon} className={styles['work-icon']} alt="work-icon1"/>}
+        >
+            <h3 className="vertical-timeline-element-title">{item.title}</h3>
+            <p>{item.description}</p>
+        </VerticalTimelineElement>
+    ))}
+</VerticalTimeline>
     );
 };
 
