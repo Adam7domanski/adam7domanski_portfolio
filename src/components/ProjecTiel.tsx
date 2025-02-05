d57d1688e0623ca633b9798a1bec0623f82e3cce
Adam7domanski <adam7domanski@gmail.com>
2025-02-06 00:14:12 +0100

    ProjectTiel component updated to follow 'component as a function principle' and imports reorganized to minimize boiler plate code

--- src/components/ProjecTiel.tsx
+++ src/components/ProjecTiel.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import styles from '../styles/ProjectTiel.module.css';
 
 interface ProjectTielProps {
@@ -7,27 +7,22 @@ interface ProjectTielProps {
     description: string;
     techstackElements: string;
 }
-class ProjecTiel extends Component<ProjectTielProps> {
-    render() {
-        const { media, title, description, techstackElements } = this.props;
 
-        return (
-            <div className={styles.projectTiel}>
-                <img src={media} alt={title} className={styles.projectMedia} />
-                <div className={styles.projectContent}>
-                    <h3 className={styles.projectTitle}>{title}</h3>
-                    <p className={styles.projectDescription}>{description}</p>
-                    <div className={styles.projectTechStack}>
-                        {techstackElements.split(',').map((tech, index) => (
-                            <span key={index} className={styles.techStackElement}>
-                                {tech.trim()}
-                            </span>
-                        ))}
-                    </div>
+export default function ProjecTiel({ media, title, description, techstackElements }: ProjectTielProps) {
+    return (
+        <div className={styles.projectTiel}>
+            <img src={media} alt={title} className={styles.projectMedia} />
+            <div className={styles.projectContent}>
+                <h3 className={styles.projectTitle}>{title}</h3>
+                <p className={styles.projectDescription}>{description}</p>
+                <div className={styles.projectTechStack}>
+                    {techstackElements.split(',').map((tech, index) => (
+                        <span key={index} className={styles.techStackElement}>
+                            {tech.trim()}
+                        </span>
+                    ))}
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default ProjecTiel;
